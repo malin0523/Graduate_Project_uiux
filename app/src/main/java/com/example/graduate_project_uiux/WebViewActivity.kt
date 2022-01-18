54ab338fdea32c87e109c653eee65a52aa24ee68
malin0523 <malin0523@naver.com>
2022-01-18 21:15:55 +0900

    다음 지도검색 api를 활용한 우편번호 검색 및 텍스트 붙이기 기능

---
+++ app/src/main/java/com/example/graduate_project_uiux/WebViewActivity.kt
@@ -0,0 +1,39 @@
+import android.R
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.webkit.JavascriptInterface
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+
+class WebViewActivity : AppCompatActivity() {
+    private var browser: WebView? = null
+
+    internal inner class MyJavaScriptInterface {
+        @JavascriptInterface
+        fun processDATA(data: String?) {
+            val extra = Bundle()
+            val intent = Intent()
+            extra.putString("data", data)
+            intent.putExtras(extra)
+            setResult(RESULT_OK, intent)
+            finish()
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.WebView)
+        browser = findViewById<View>(R.id.webView) as WebView
+        browser!!.settings.javaScriptEnabled = true
+        browser!!.addJavascriptInterface(MyJavaScriptInterface(), "Android")
+        browser!!.webViewClient = object : WebViewClient() {
+            override fun onPageFinished(view: WebView, url: String) {
+                browser!!.loadUrl("javascript:sample2_execDaumPostcode();")
+            }
+        }
+        browser!!.loadUrl("http://서버주소/daum.html")
+    }
+}
